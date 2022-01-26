fdfd3555d003f1198a2301618f3e6bee6e7a9f72
r04dw4rri0r <30304129+r04dw4rri0r@users.noreply.github.com>
2022-01-25 23:16:54 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+HOST = "irc.twitch.tv"                        # This is Twitchs IRC server
+PORT = 6667                                   # Twitchs IRC server listens on port 6767
+NICK = "DICKS"               # Twitch username your using for your bot
+PASS = "Hmm" # your Twitch OAuth token
+CHAN = "#roadwarriortv"                       # the channel you want the bot to join.
+RATE = (20/30)                                # messages per second
+BAN_PAT = [
+
+]
+
+TIMEONE_PAT = [
+
+]
